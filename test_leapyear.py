cc1f6a9adb52bc900534e26f9c05ecc2465921ca
Jackrifleman <Jeubank100@gmail.com>
2021-03-07 16:28:53 -0800

    Added first test to leap year
    This test determines if the program can correctly check if the given input is evenly divisible by 4

---
+++ test_leapyear.py
@@ -0,0 +1,15 @@
+#Jackson Eubank
+#CS362
+#HW7 - Leap Year Testing
+
+import unittest;
+import leapyear;
+
+class UnitTest(unittest.TestCase):
+    def test_modulo_four(self):
+        testNumb = leapyear.parseYear(4);
+        self.assertEqual(testNumb,true);
+
+
+if (__name__ == '__main__'):
+    unittest.main();
